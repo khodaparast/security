ec9415d9c3405dd20cc2238a9e8cf10d79cc1adb
khodaparast <f.p.khodaparast@gmail.com>
2021-04-22 15:29:38 -0300

    adding CMakeLists.txt

---
+++ cryptography/openSSL/CMakeFiles/FKHEncryption.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/FKHEncryption.dir/src/test_ssl_enc.cc.o"
+  "FKHEncryption"
+  "FKHEncryption.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/FKHEncryption.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
